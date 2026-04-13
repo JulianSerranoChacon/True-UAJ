034abca0bc7949d300b014416d0025e6f20d0146
PablooMS <pamarc05@ucm.es>
2026-04-13 13:23:07 +0200

    Lo que quedaba de serializacion

--- Assets/Scripts/Tracking/Serializacion/JsonSerializer.cs
+++ Assets/Scripts/Tracking/Serializacion/JsonSerializer.cs
@@ -18,9 +18,12 @@ public class JsonSerializer : ISerializer
         while (!evQ.Empty())
         {
             result += evQ.Read().ToJson();
+
+            if (!evQ.Empty())
+                result += ",\n";
         }
 
-        result += "}";
+        result += "\n}";
 
         return result;
     }
