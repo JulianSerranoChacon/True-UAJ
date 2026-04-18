3e9f457ad6799cf487a230ef676a0e3ca39db1ac
PablooMS <pamarc05@ucm.es>
2026-04-18 19:14:53 +0200

    Pruebas del python

--- Analisis/filespy/jugadores.py
+++ Analisis/filespy/jugadores.py
@@ -1,12 +1,26 @@
 #En esta celda es donde haremos el for por cada jugador (sessionID) distinto.
 #Si lo hacemos bien podemos usar las librerias para ahorrarnos el trabajo.
 
-jug = 10; #Valor de jugadores arbitrario
-
-for i in range(jug):
+#Valor de jugadores arbitrario
+for i in range(10):
     dataJug = ['name7', 0, 0, 0, 0]
+
+    print("5")
     #Loopear todos los eventos, y tener un switch para que segun event-type haga una cosa u otra.
     #BUCLE
+    for j in i:
         #SWITCH
+        j
+        match i:
+            case "":
+                break
+            case "1":
+                break
+            case _:
+                break
+        algomas
+    
     #Posteriormente anexionamos jugoslavia (es broma solo anexionamos a tu madre)
     singleDf.loc[i] = dataJug;
+
+
