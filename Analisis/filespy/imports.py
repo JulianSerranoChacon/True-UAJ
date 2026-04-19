dc60ab1bae7fd592cc03b118911c958ea9a90599
PablooMS <pamarc05@ucm.es>
2026-04-19 21:41:11 +0200

    Metricas y grafos to guapos

---
+++ Analisis/filespy/imports.py
@@ -0,0 +1,10 @@
+#Imports:
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import scipy.stats as stats
+import os
+import json
+
+%matplotlib inline
