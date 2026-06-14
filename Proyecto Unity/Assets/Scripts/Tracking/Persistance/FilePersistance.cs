94753c3f267c8e7ce245489305fa9db34fc7cca1
Chao <114423476+ChaoIsBalling@users.noreply.github.com>
2026-06-14 19:10:41 +0200

    Se ha separado las funciones del File Persistance del Ipersistance
    Co-Authored-By: Javalonso1 <123580168+Javalonso1@users.noreply.github.com>

--- Proyecto Unity/Assets/Scripts/Tracking/Persistance/FilePersistance.cs
+++ Proyecto Unity/Assets/Scripts/Tracking/Persistance/FilePersistance.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -7,7 +8,36 @@ using UnityEngine;
 //Peristencia en disco
 public class FilePersistance : IPersistance {
 
+    private StreamWriter sw;
+    private bool fileNull = false;
+    override  public void Init(ISerializer Is, String fileName)
+    {
+        serializer = Is;
+        try
+        {
+            string dirPath = Path.Combine(Application.dataPath, "../../Datos Telemetria");
 
+            if (!Directory.Exists(dirPath))
+                Directory.CreateDirectory(dirPath);
+
+            string fullPath = Path.Combine(dirPath, fileName);
+
+            sw = new StreamWriter(fullPath + ".json", true);
+        }
+        catch (ArgumentNullException e)
+        {
+            Debug.LogError(e.Message);
+            fileNull = true;
+        }
+    }
+    override public void Close()
+    {
+        if (!fileNull && sw != null)
+        {
+            sw.Close();
+            sw = null;
+        }
+    }
     override public void Flush(CircularBuffer<TrackerEvent>evq)
     {
         if (!fileNull)
