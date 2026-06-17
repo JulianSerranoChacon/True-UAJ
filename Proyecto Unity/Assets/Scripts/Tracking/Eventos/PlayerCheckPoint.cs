45e7a6e44eee94d649acd017514104d959f20c02
VerXxuxZ <juserr04@ucm.es>
2026-06-17 20:41:06 +0200

    levelId eliminado

--- Proyecto Unity/Assets/Scripts/Tracking/Eventos/PlayerCheckPoint.cs
+++ Proyecto Unity/Assets/Scripts/Tracking/Eventos/PlayerCheckPoint.cs
@@ -1,16 +1,9 @@
 public class PlayerCheckPoint : TrackerEvent
 {
     private int levelID;
-    private int checkpointID;
 
     #region Bean
     public PlayerCheckPoint() { }
-
-    public int LevelID
-    {
-        get { return levelID; }
-        set { levelID = value; }
-    }
     
     public int CheckpointID
     {
@@ -21,6 +14,6 @@ public class PlayerCheckPoint : TrackerEvent
 
     public override string ToJson()
     {        
-        return "{\n\"type\": \"playerCP\"\n" + this.parentToJson() + ",\n\"levelID\": " + levelID + ",\n\"checkpointID\": "+ checkpointID + "\n}\n,";
+        return "{\n\"type\": \"playerCP\"\n" + this.parentToJson() + ",\n\"levelID\": " + levelID + ",\n}\n,";
     }
 }
